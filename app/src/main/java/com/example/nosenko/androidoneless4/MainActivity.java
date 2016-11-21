87b2e2f7f08daff16ef0f980959544d2eb52b5b9
Alexey Nosenko <Onyx_2009@li.ru>
2016-11-21 21:53:32 +0300

    Осталось тестирование и интерфейс

--- app/src/main/java/com/example/nosenko/androidoneless4/MainActivity.java
+++ app/src/main/java/com/example/nosenko/androidoneless4/MainActivity.java
@@ -1,16 +1,18 @@
 package com.example.nosenko.androidoneless4;
 
+import android.support.v4.media.session.PlaybackStateCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
-// запрет ввода текста
 // поправить интерфейс, два стиля
-// сохранять данные при перевороте
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private enum actions {SUM,
@@ -18,17 +20,61 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                           MULTIPLICATION,
                           DIVISION};
 
-    private Button btnSum;
-    private Button btnDifference;
-    private Button btnMultiplication;
-    private Button btnDivision;
-    private Button btnEqually;
 
-    final String MASK = "%.3f";
+    private EditText etValueA = null;
+    private EditText etValueB = null;
+    private TextView tvSolution = null;
+    private Button btnSum = null;
+    private Button btnDifference = null;
+    private Button btnMultiplication = null;
+    private Button btnDivision = null;
+    private Button btnEqually = null;
 
-    actions action;
+    private float value_a = 0;
+    private float value_b = 0;
+    private String solution = null;
 
-    private void Init(){
+    private final String MASK = "%.3f";
+
+    private actions action = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etValueA = (EditText)findViewById(R.id.etValueA);
+        etValueA.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        etValueA.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                value_a = Float.parseFloat(etValueA.getText().toString());
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        etValueB = (EditText)findViewById(R.id.etValueB);
+        etValueB.setInputType(InputType.TYPE_CLASS_NUMBER | InputType.TYPE_NUMBER_FLAG_DECIMAL);
+        etValueB.addTextChangedListener(new TextWatcher(){
+            @Override
+            public void afterTextChanged(Editable s) {
+                value_b = Float.parseFloat(etValueB.getText().toString());
+            }
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+        });
+
+        tvSolution = (TextView)findViewById(R.id.tvSolution);
+
         btnSum = (Button) findViewById(R.id.btnSum);
         btnSum.setOnClickListener(this);
 
@@ -43,13 +89,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         btnEqually = (Button) findViewById(R.id.btnEqually);
         btnEqually.setOnClickListener(this);
-    }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Init();
+        if (savedInstanceState != null){
+            value_a = savedInstanceState.getFloat("value_a");
+            etValueA.setText(String.format(MASK, value_a));
+
+            value_b = savedInstanceState.getFloat("value_b");
+            etValueB.setText(String.format(MASK, value_b));
+
+            action = (actions) savedInstanceState.getSerializable("action");
+            setActionText();
+
+            solution = savedInstanceState.getString("solution");
+            tvSolution.setText(solution);
+        }
     }
 
     private String calculated(float a, float b, actions action){
@@ -73,26 +126,35 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return "";
     }
 
+    private void setActionText(){
+        TextView tvAction = (TextView)findViewById(R.id.tvAction);
+        switch (action){
+            case SUM:
+                tvAction.setText("+");
+                break;
+            case DIFFERENCE:
+                tvAction.setText("-");
+                break;
+
+            case MULTIPLICATION:
+                tvAction.setText("*");
+                break;
+
+            case DIVISION:
+                tvAction.setText("/");
+                break;
+        }
+    }
+
     @Override
     public void onClick(View v) {
         Log.d("onClick", "onClick");
         if (v.getId() == btnEqually.getId()){
             Log.d("onClick", "btnEqually");
-            EditText editText = (EditText)findViewById(R.id.etValueA);
-            float a = Float.parseFloat(editText.getText().toString());
-
-            editText = (EditText)findViewById(R.id.etValueB);
-            float b = Float.parseFloat(editText.getText().toString());
-
-            TextView tvSolution = (TextView)findViewById(R.id.tvSolution);
-            tvSolution.setText(calculated(a, b, action));
+            solution = calculated(value_a, value_b, action);
+            tvSolution.setText(solution);
         }
         else{
-            TextView tvAction = (TextView)findViewById(R.id.tvAction);
-            Button btn = (Button)v;
-            tvAction.setText(btn.getText());
-
-
             if (v.getId() == btnSum.getId()){
                 Log.d("onClick", "btnSum");
                 action = actions.SUM;
@@ -112,6 +174,16 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Log.d("onClick", "btnDivision");
                 action = actions.DIVISION;
             }
+
+            setActionText();
         }
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        savedInstanceState.putFloat("value_a", value_a);
+        savedInstanceState.putFloat("value_b", value_b);
+        savedInstanceState.putSerializable("action", action);
+        savedInstanceState.putString("solution", solution);
     }
 }
